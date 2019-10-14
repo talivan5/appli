4a22a1d9637a13c00c76ee1f1e2fe5f250d3d824
talivan5 <talivan51987@gmail.com>
2019-10-14 10:08:42 -0400

    subir los cambios

---
+++ database/migrations/2019_10_08_151839_create_personas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("nombres");
+            $table->string("apellidos");
+            $table->date("fecha_nacimiento");
+            $table->integer("estado");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
